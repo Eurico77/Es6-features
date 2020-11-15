5c8082afbf3eab14d6b77634c13a72a543be7fb6
eurico77 <euricomagalhaes61@gmail.com>
2020-11-15 12:30:16 -0300

    primeiro commit

---
+++ src/modules/functions/basico.js
@@ -0,0 +1,18 @@
+// Declaração de função function declaration
+
+function declaration(a, b, c) {
+ return a + b + c;
+}
+
+console.log(declaration(1, 4, 6));
+
+
+// expressão de função function expression
+
+const expression = function() {
+   console.log('expresssion')
+}
+
+expression();
+
+
---
+++ src/modules/functions/fn_1.js
@@ -0,0 +1,37 @@
+const bomDia = function() {
+   console.log('bom dia ');
+}
+
+const boaTarde = function() {
+   console.log('boa tarde');
+}
+
+
+function executarQualquerCoisa(fn) {
+
+   typeof fn === 'function' ? fn() : 'Não é função';
+   // if(typeof fn == 'function') {
+   //   fn()
+   // }
+}
+
+executarQualquerCoisa(3)
+executarQualquerCoisa(boaTarde)
+executarQualquerCoisa(bomDia)
+
+
+
+const potencia2 = base1 => exp1 => Math.pow(base1, exp1)
+
+// 
+function potencia(base) {
+   return function(exp) {
+      return Math.pow(base, exp)
+   }
+}
+
+
+const result2 = potencia2(2)
+console.log(result2(8));
+const result = potencia(2)
+console.log(result(8));
