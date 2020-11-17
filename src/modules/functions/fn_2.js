9b66ebac8687623f11221a0b08e500e7656f3089
eurico77 <euricomagalhaes61@gmail.com>
2020-11-17 13:11:50 -0300

    usando map

--- src/modules/functions/fn_2.js
+++ src/modules/functions/fn_2.js
@@ -8,14 +8,15 @@ saudacao("eurico");
 
 
 // exemplo com map
-const somar = (numeros) => {
+const somar = (...numeros) => {
+   console.log(` me diz se isso é um array valido ${Array.isArray(numeros)}`);
   let total = 0;
   numeros.map((n) => {
     total += n;
-    console.log(n);
+    console.log(`o N esta percorrendo o indicia atual ${n}`);
   });
 
   return total;
 };
 
-console.log(somar([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+console.log(`O valor total é ${somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)}`);
