70e95ad51a5584f87833a29cf2e990e66d814524
eurico77 <euricomagalhaes61@gmail.com>
2020-11-17 22:31:51 -0300

    setInterval com callbacks

---
+++ src/modules/callbacks/callback_2.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+ 
+const caminho = path.join(`${__dirname}/utils/data.txt`)
+
+function exibir(err, content) {
+   console.log(content.toString());
+}
+
+fs.readFile(caminho, exibir)
+
+console.log('inicio sync');
+const result = fs.readFileSync(caminho)
+console.log(result.toString());
+console.log('fim sync');
