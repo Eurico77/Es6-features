adc7edac265cafc8e1d9576ba5f8282b9c752fb8
eurico77 <euricomagalhaes61@gmail.com>
2020-11-17 22:09:18 -0300

    setInterval com callbacks

--- src/modules/callbacks/callback_1.js
+++ src/modules/callbacks/callback_1.js
@@ -4,7 +4,13 @@ function exec(fn, n1, n2) {
 }
 
 const somar = (n1, n2) => n1 + n2;
-const somarNoTerminal = somar;
+const subtrair = (n1, n2) => n1 - n2;
 
+console.log(exec(somar, 20, 30));
 
-console.log(exec(somarNoTerminal, 20, 30));
+const r2 = exec(subtrair, 2, 5)
+console.log(r2);
+
+const cb = () => console.log('testando callbacks com setInterval');
+
+setInterval(cb, 1000)
