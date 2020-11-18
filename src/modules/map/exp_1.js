1a65a7e46d90117e2b666158238ca9223443f739
eurico77 <euricomagalhaes61@gmail.com>
2020-11-17 23:21:19 -0300

    map, e params suportados

--- src/modules/map/exp_1.js
+++ src/modules/map/exp_1.js
@@ -3,6 +3,7 @@
 const n = [3, 4, 5, 6, 7, 8, 9];
 console.log(n);
 
+
 const result = n.map((el)=> el * 2 )
 console.log(result);
 
@@ -12,3 +13,8 @@ const exp = ['eurico', 'ana', 'iris']
 
 const r = exp.map((el) => el.toLocaleUpperCase())
 console.log(r);
+
+//parametros suportados no map
+const params = [2,4,5,6,7,8,66,3,2324,342]
+const r2 = params.map((valor, index, array) => console.log(`o valor ${valor * 2}
+, o index ${index}, o array ${array}`))
