3f6f138137183d48ba21ac4a563676cdfa0b8665
eurico77 <euricomagalhaes61@gmail.com>
2020-11-19 00:11:33 -0300

    reduce desafio

--- src/modules/FilterMapReduce/chalenges/index.js
+++ src/modules/FilterMapReduce/chalenges/index.js
@@ -4,11 +4,30 @@ const carrinho = [
   { nome: "Caneta", qtd: 3, preco: 4.59, fragil: true },
   { nome: "Bola de Futebol", qtd: 100, preco: 20.34, fragil: true },
   { nome: "SSd", qtd: 3, preco: 200, fragil: true },
-  { nome: "Ps4", qtd: 2, preco: 3232.23, fragil: true },
+  { nome: "Ps4", qtd: 2, preco: 3232.23, fragil: false },
   { nome: "Bola de Basquete", qtd: 2, preco: 23.52, fragil: true },
   { nome: "Fone de Ouvido", qtd: 3, preco: 234.3, fragil: true },
-  { nome: "Memoria RAM", qtd: 4, preco: 623.55, fragil: true },
+  { nome: "Memoria RAM", qtd: 4, preco: 623.55, fragil: false },
   { nome: "Leite em pó", qtd: 34, preco: 23.54, fragil: true },
-  { nome: "Nutela", qtd: 4, preco: 23.12, fragil: true },
-  { nome: "Impressora", qtd: 0, preco: 3333.3, fragil: true },
+  { nome: "Nutela", qtd: 4, preco: 23.12, fragil: false },
+  { nome: "Impressora", qtd: 4, preco: 3333.3, fragil: true },
 ];
+// desafio 
+// 1. apenas filtrar produtos com o status fragil: true
+// 2. pegar qtd, preço
+// 3. fazer a media dos totais 
+
+const result = carrinho
+   .filter(p => !p.fragil)
+   .map(p => p.preco * p.qtd)
+   .reduce((acc, el) => {
+      return {
+         qtd: acc.qtd + 1,
+         total: acc.total + el,
+         media: 0
+      }
+
+   }, {qtd: 0, total: 0, media: 0})
+
+
+console.log(result);
